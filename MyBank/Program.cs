421788be77ad8457c1513d74dda3d6ae8fe437d9
James Monks <James.Monks@academytrainee.com>
2020-11-27 12:35:34 +0000

    first things added

---
+++ MyBank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("<name>", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            var account2 = new BankAccount("<another name>", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+        }
+    }
+}
